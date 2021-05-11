f396e2a99fac8eb68a074d344e2cf98c4dfa0010
matthew <mematthew123@gmail.com>
2021-05-10 20:47:24 -0600

    updated info in Main Controller, Need to create security Package

--- src/main/java/com/meritamerica/assignment7/controllers/MainController.java
+++ src/main/java/com/meritamerica/assignment7/controllers/MainController.java
@@ -1,5 +1,65 @@
 package com.meritamerica.assignment7.controllers;
 
-public class MainController {
+import java.util.List;
 
-}
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meritamerica.assignment7.models.AccountHolder;
+import com.meritamerica.assignment7.models.AccountHoldersContactDetails;
+import com.meritamerica.assignment7.service.AccountHolderService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.meritamerica.assignment7.security.util.JwtUtil;
+
+@RestController
+public class MainController {
+	
+	
+	@Autowired
+	private AuthenticationManager authenticationManager; 
+	@Autowired
+	private JwtUtil JwtTokenUtil;
+	
+	@Autowired
+	private MapReactiveUserDetailsService userDetailsService; // needs seCurity package created 
+	
+	@GetMapping("/")
+	public String Home () {
+		return " Merit Credit Union Appreicates You";
+	}
+//	@PostMapping("/authenticate")
+////	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+//			throws Exception {
+//
+//		try {
+//			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+//					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+//		} catch (BadCredentialsException e) {
+//			throw new Exception("Incorrect username or password", e);
+//		}
+//
+//		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+//
+//		final String jwt = jwtTokenUtil.generateToken(userDetails);
+//
+//		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+//	}
+//}
--- src/main/java/com/meritamerica/assignment7/controllers/UserController.java
+++ src/main/java/com/meritamerica/assignment7/controllers/UserController.java
@@ -1,5 +1,24 @@
 package com.meritamerica.assignment7.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.meritamerica.assignment7.models.AccountHolder;
+import com.meritamerica.assignment7.models.AccountHoldersContactDetails;
+import com.meritamerica.assignment7.service.AccountHolderService;
+
 public class UserController {
 
 }
