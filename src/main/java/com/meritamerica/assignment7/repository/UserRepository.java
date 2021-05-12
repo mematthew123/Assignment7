59bf34c407f2218ed5adb0a45d6baf2255dd8aac
matthew <mematthew123@gmail.com>
2021-05-11 18:15:45 -0600

    uCreated user model class,Repo Class, Service Class

---
+++ src/main/java/com/meritamerica/assignment7/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.meritamerica.assignment7.repository;
+
+public class UserRepository {
+
+}
---
+++ src/main/java/com/meritamerica/assignment7/service/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.meritamerica.assignment7.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.meritamerica.assignment7.models.User;
+import com.meritamerica.assignment7.repository.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+}
